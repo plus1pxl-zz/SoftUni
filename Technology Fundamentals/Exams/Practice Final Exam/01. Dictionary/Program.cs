bb0b72279608c7b25243cf7534771fad7cca9460
plus1pxl <plus1pxl@users.noreply.github.com>
2019-04-13 19:52:57 +0300

    Added Tech Fundamentals Final Exam Practice

---
+++ Technology Fundamentals/Exams/Practice Final Exam/01. Dictionary/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<string, List<string>>();
+
+            string[] tokens = Console.ReadLine().Split(" | ").ToArray();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                string[] wordDefinition = tokens[i].Split(": ").ToArray();
+
+                string word = wordDefinition[0];
+                string definition = wordDefinition[1];
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                    words[word].Add(definition);
+                }
+                else
+                {
+                    words[word].Add(definition);
+                }
+            }
+
+            string[] wordsToShow = Console.ReadLine().Split(" | ").ToArray();
+
+            foreach (var word in wordsToShow)
+            {
+                foreach (var kvp in words)
+                {
+                    if (kvp.Key == word)
+                    {
+                        Console.WriteLine($"{kvp.Key}:");
+                        foreach (var w in kvp.Value.OrderByDescending(x => x.Length))
+                        {
+                            Console.WriteLine($" -{w}");
+                        }
+                    }
+                }
+            }
+
+            string finalInput = Console.ReadLine();
+            if (finalInput.ToLower() == "list")
+            {
+                var output = String.Join(" ", words.Keys.OrderBy(x => x).ToArray());
+                Console.WriteLine(output);
+            }
+            else if (finalInput.ToLower() == "end")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/Exams/Practice Final Exam/02. Deciphering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] cypher = Console.ReadLine().Split().ToArray();
+            string pattern = @"[d-z{}|#]*";
+
+            // check for matches
+            Match match = Regex.Match(input, pattern);
+            if (match.Length != input.Length)
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+            else
+            {
+                char[] inputAsChars = input.ToCharArray();
+                for (int i = 0; i < inputAsChars.Length; i++)
+                {
+                    inputAsChars[i] = (char)((int)inputAsChars[i] - 3);
+                }
+                string processed = new string(inputAsChars);
+                Console.WriteLine(processed.Replace(cypher[0], cypher[1]));
+
+            }
+        }
+    }
+}
