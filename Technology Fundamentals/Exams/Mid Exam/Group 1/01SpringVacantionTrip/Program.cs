2b4f2b5152e869346645386acb37db19560e3b7d
plus1pxl <plus1pxl@users.noreply.github.com>
2019-04-15 19:38:24 +0300

    Added Tech C# Mid Exam Group 1

---
+++ Technology Fundamentals/Exams/Mid Exam/Group 1/01SpringVacantionTrip/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _01SpringVacantionTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+
+
+            int days = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            int persons = int.Parse(Console.ReadLine());
+            double fuelPricePerKm = double.Parse(Console.ReadLine());
+            double foodExpensesPerDay = double.Parse(Console.ReadLine());
+            double roomPricePerDay = double.Parse(Console.ReadLine());
+            int kmTravelled = 0;
+            double currentExpenses = 0;
+
+            if (persons > 10)
+            {
+                roomPricePerDay *= 0.75;
+            }
+
+            currentExpenses = (foodExpensesPerDay + roomPricePerDay) * days * persons;
+
+            for (int i = 1; i <= days; i++)
+            {
+                kmTravelled = int.Parse(Console.ReadLine());
+                currentExpenses += kmTravelled * fuelPricePerKm;
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    currentExpenses += currentExpenses * 0.4;
+                }
+                if (i % 7 == 0)
+                {
+                    currentExpenses -= currentExpenses / persons;
+                }
+                if (currentExpenses > budget)
+                {
+                    Console.WriteLine($"Not enough money to continue the trip. You need {(currentExpenses - budget):F2}$ more.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"You have reached the destination. You have {(budget - currentExpenses):F2}$ budget left.");
+
+
+
+        }
+    }
+}
---
+++ Technology Fundamentals/Exams/Mid Exam/Group 1/03LastStop/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03LastStop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> paintings = Console.ReadLine().Split(" ").ToList();
+            string input = Console.ReadLine();
+            var command = input.Split(" ");
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "Change")
+                {
+                    if (paintings.Contains(command[1]))
+                    {
+                        for (int i = 0; i < paintings.Count; i++)
+                        {
+                            if (paintings[i] == command[1])
+                            {
+                                paintings[i] = command[2];
+                            }
+                        }
+                    }
+
+                }
+
+                else if (command[0] == "Hide")
+                {
+                    if (paintings.Contains(command[1]))
+                    {
+                        paintings.Remove(command[1]);
+                    }
+                }
+
+                else if (command[0] == "Switch")
+                {
+                    var tmp = command[1];
+                    var index1 = 0;
+                    var index2 = 0;
+                    if (paintings.Contains(command[1]) && paintings.Contains(command[2]))
+                    {
+                        for (int i = 0; i < paintings.Count; i++)
+                        {
+                            if (paintings[i] == command[1])
+                            {
+                                index1 = i;
+                            }
+                            else if (paintings[i] == command[2])
+                            {
+                                index2 = i;
+                            }
+                        }
+                        paintings[index1] = paintings[index2];
+                        paintings[index2] = tmp;
+                    }
+
+                }
+
+                else if (command[0] == "Insert")
+                {
+                    int index = int.Parse(command[1]);
+                    if (paintings.Count > index && index > -1)
+                    {
+                        paintings.Insert(index + 1, command[2]);
+                    }
+                }
+
+                else if (command[0] == "Reverse")
+                {
+                    paintings.Reverse();
+                }
+                input = Console.ReadLine();
+                command = input.Split(" ");
+            }
+
+            foreach (var painting in paintings)
+            {
+                Console.Write($"{painting} ");
+            }
+        }
+    }
+}
