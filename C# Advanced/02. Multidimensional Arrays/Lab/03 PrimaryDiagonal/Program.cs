dbb7be2dd51536c9c635e8564f8e0621a1750436
plus1pxl <plus1pxl@users.noreply.github.com>
2019-06-23 14:03:54 +0300

    Added C# Advanced/02. Multidimensional Arrays/Lab/

---
+++ C# Advanced/02. Multidimensional Arrays/Lab/03 PrimaryDiagonal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dim = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[dim, dim];
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = tokens[j];
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Advanced/02. Multidimensional Arrays/Lab/04 SymbolInMatrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dim = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[dim, dim];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                char[] tokens = Console.ReadLine()
+                    .ToCharArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = tokens[j];
+                }
+            }
+
+            char charToMatch = Console.ReadLine()[0];
+            bool match = false;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] == charToMatch)
+                    {
+                        match = true;
+                        Console.WriteLine((i, j));
+                    }
+                }
+            }
+
+            if (!match)
+            {
+                Console.WriteLine($"{charToMatch} does not occur in the matrix");
+            }
+        }
+    }
+}
