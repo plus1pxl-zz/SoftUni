36ada980af20d5b52f291300ab01042bb53cbe35
plus1pxl <plus1pxl@users.noreply.github.com>
2019-07-10 14:17:15 +0300

    Updated "C# Advanced\05. Functional Programming\Excersise"

---
+++ C# Advanced/05. Functional Programming/Excersise/FunctionalProgramingExc/09 ListOfPredicates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperBound = int.Parse(Console.ReadLine());
+            List<int> numbers = Enumerable.Range(1, upperBound).ToList();
+            int[] inputNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Distinct()
+                .ToArray();
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+            foreach (var currentNumber in inputNumbers)
+            {
+                predicates.Add(x => x % currentNumber == 0);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                foreach (var currentPredicate in predicates)
+                {
+                    if (!currentPredicate(numbers[i]))
+                    {
+                        numbers.Remove(numbers[i]);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ C# Advanced/05. Functional Programming/Excersise/FunctionalProgramingExc/10 PredicateParty!/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10_PredicateParty_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine()
+                .Split()
+                .ToList();
+            string input = Console.ReadLine();
+            while (input != "Party!")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string filterCommand = tokens[1];
+                string criteria = tokens[2];
+
+                if (command == "Remove")
+                {
+                    List<string> guestsToRemove = new List<string>();
+                    if (filterCommand == "StartsWith")
+                    {
+                        guests.RemoveAll(g => g.StartsWith(criteria));
+                    }
+                    else if (filterCommand == "EndsWith")
+                    {
+                        guests.RemoveAll(g => g.EndsWith(criteria));
+                    }
+                    else if (filterCommand == "Length")
+                    {
+                        guests.RemoveAll(g => g.Length == int.Parse(criteria));
+                    }
+
+                }
+                else if (command == "Double")
+                {
+                    List<string> guestsToAdd = new List<string>();
+                    if (filterCommand == "StartsWith")
+                    {
+                        guestsToAdd = guests.Where(g => g.StartsWith(criteria)).ToList();
+                    }
+                    else if (filterCommand == "EndsWith")
+                    {
+                        guestsToAdd = guests.Where(g => g.EndsWith(criteria)).ToList();
+                    }
+                    else if (filterCommand == "Length")
+                    {
+                        guestsToAdd = guests.Where(g => g.Length == int.Parse(criteria)).ToList();
+                    }
+                    foreach (var name in guestsToAdd)
+                    {
+                        int index = guests.IndexOf(name);
+                        guests.Insert(index + 1, name);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(guests.Any() ? $"{string.Join(", ", guests)} are going to the party!" : "Nobody is going to the party!");
+        }
+
+    }
+}
